256587793b196721a0f6302b4a63e76fa343f2e0
Michael Yapi <myapi@edane.org>
2024-04-23 16:17:37 +0000

    Update init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
+import { plugin, defaultConfig } from '@formkit/vue'
+import { fr } from '@formkit/i18n'
 import App from './App.vue'
 import router from "./router"
 
 const app = createApp(App)
 app.use(router)
+app.use(plugin, defaultConfig({
+    locales: { fr },
+    locale: 'fr',
+}))
 app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
     {
         path: '/',
+        redirect: "/login",
         component: () => import('@/layouts/MainLayout.vue'),
         children: [
             {
@@ -10,6 +11,11 @@ const routes = [
                 component: () => import('@/views/HomeView.vue')
             }
         ]
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: () => import('@/views/LoginView.vue')
     }
 ]
 
